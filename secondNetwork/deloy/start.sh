f401c8502323b60c019465d074a3473abb999571
kongxiangyue <513001363@qq.com>
2021-02-23 20:44:57 +0800

    [M] 根据通道配置文件生成通道

--- secondNetwork/deloy/start.sh
+++ secondNetwork/deloy/start.sh
@@ -4,7 +4,7 @@ SYS_CHANNEL="sys-channel"
 CHANNEL_NAME="assetschannel"
 echo $SYS_CHANNEL
 
-echo "一、环境清理"
+echo "一 环境清理"
 mkdir -p config
 mkdir -p crypto-config
 mkdir -p channel-artifacts
@@ -15,40 +15,50 @@ rm -fr channel-artifacts/*
 echo "清理完毕"
 
 
-echo "二、生成证书和起始区块信息"
+echo "二 生成证书和起始区块信息"
 cryptogen generate --config=./crypto-config.yaml
 #configtxgen -profile TwoOrgsOrdererGenesis -outputBlock -channelID  ./channel-artifacts/genesis.block
 # -channelID $SYS_CHANNEL
 configtxgen -profile OneOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
 
+
 echo "三 生成通道配置文件"
 mkdir -p channel-artifacts
 configtxgen -profile TwoOrgsChannel \
--outputCreateChannelTx ./channel-artifacts/channel.tx \
+-outputCreateChannelTx ./channel-artifacts/$CHANNEL_NAME.tx \
 -channelID $CHANNEL_NAME
+
 
 echo "四 生成锚节点配置文件"
 echo "生成Organization0MSP锚节点配置文件"
 configtxgen -profile TwoOrgsChannel \
 -outputAnchorPeersUpdate ./channel-artifacts/Organization1MSPanchors.tx \
 -channelID $CHANNEL_NAME \
--asOrg Organization0MSP
+-asOrg Org0MSP
 
 
 echo "生成Organization1MSP锚节点配置文件"
 configtxgen -profile TwoOrgsChannel \
 -outputAnchorPeersUpdate ./channel-artifacts/Organization1MSPanchors.tx \
 -channelID $CHANNEL_NAME \
--asOrg Organization1MSP
+-asOrg Org1MSP
 
 echo "生成Organization2MSP锚节点配置文件"
 configtxgen -profile TwoOrgsChannel \
 -outputAnchorPeersUpdate ./channel-artifacts/Organization1MSPanchors.tx \
 -channelID $CHANNEL_NAME \
--asOrg Organization2MSP
+-asOrg Org2MSP
 
 echo "五 启动docker容器"
 docker-compose -f docker-compose-cli.yaml up -d
+sleep 10
+
+echo "打印正在运行的docker容器"
+docker ps -a
+
+
+echo "六 根据通道配置文件生成通道"
+docker exec cli peer channel create -o orderer.educhain.accurchain.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/$CHANNEL_NAME.tx
 
 
 
